1e217dc33047835dd5ee5560e986207cf82d6677
Vlad <vlad.miryaha@gmail.com>
2018-10-09 01:42:04 +0300

    fixit

--- split.c
+++ split.c
@@ -7,6 +7,10 @@ char* ScanStr(char* a, int buff)
 {
     int i = 1;
     fgets(a, buff, stdin);
+    // Если честно, то код ниже выглядит очень сложно и нет уверенности, что работает корректно
+    // Задайте buff = 1 и проверьте работоспособность.
+    // Я бы предложил конечно же добаться работоспособности, а потом удалить этот кусок, и пользоваться просто gets,
+    // предполагая, что больше 1024 строки никто вводить не будет.
     while (!((a[strlen(a) - 1] == 0) ^ (a[strlen(a) - 1] == '\n')))
     {
         ++i;
@@ -36,13 +40,20 @@ int main(void)
     char* string;
     string = (char*) malloc (buff);
     string = ScanStr(string, buff);
+    
+    // Кажется, что произошла некая путаница с указателями. Можно на семинаре обсудить.
     char*** tokens;
     tokens = (char***) calloc (1, sizeof(char**));
     char* delimeters;
     delimeters = (char*) malloc (buff);
     delimeters = ScanStr(delimeters, buff);
+    
+    // FIXIT: Точно не нужно выделять один int в куче. 
+    // Лучше просто создать переменную на стеке int tokensCount;
+    // а в ф-ю передать Split(..., &tokensCount);
     int* tokensCount;
     tokensCount = (int*) calloc(1, sizeof(int));
+    
     Split(string, delimeters, tokens, tokensCount);
     for (int i = 0; i < (*tokensCount); ++i)
     {
