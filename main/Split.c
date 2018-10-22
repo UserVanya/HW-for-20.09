aee8be9724106b97c1d2a969111e654a2d975d0b
Vlad <vlad.miryaha@gmail.com>
2018-10-22 23:34:56 +0300

    fixit

--- main/Split.c
+++ main/Split.c
@@ -5,6 +5,17 @@ void Split(char* string, char* delimeters, char*** tokens, int* tokensCount)
 {
     if(string && delimeters && tokens && tokensCount)
     {
+        // FIXIT: 1) tokensCount[0], tokens[0] сбивают с толку. мы обсуждали, что можно сделать так:
+        //  void Split(... , int* tokensCountPtr) {
+        // int tokensCount = 0;
+        // ...
+        // *tokensCountPtr = tokensCount;
+        // аналогично для tokens
+        // 2) на самом деле вы делаете излишние манипуляции с памятью. прочитайте, пожалуйста, что именно делает
+        // strtok ... кажется выделять память под temp не нужно
+        // у вас под строку (*tokens)[0] память не была выделена, но вы туда что-то копируете strcpy(tokens[0][tokensCount[0]], temp); (до цикла while)
+        // зачем делать реаллоки в цикле, если память выделяется 1 раз для каждого нового токена у вас.
+        // Но все же лучший выход прочитать внимательно про strtok и убрать все ненужные выделения памяти.
         tokensCount[0] = 0;
         char* temp = (char*) calloc (strlen(string), sizeof(char));
         temp = strtok(string, delimeters);
